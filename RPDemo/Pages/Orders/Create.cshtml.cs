0ecbf3e2fa3cb7a58d51fc0174b5237110a3e11c
Eric Carlier <eric.carlier@gmail.com>
2021-10-10 13:24:16 +0200

    adding DLL dependencies

---
+++ RPDemo/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using DataLibraryRepo.Data;
+using DataLibraryRepo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RPDemo.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public List<SelectListItem> FoodItems { get; set; }
+
+        [BindProperty]
+        public OrderModel Order { get; set; }
+        public CreateModel(IFoodData foodData,IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+        public void OnGet()
+        {
+            var food =  _foodData.GetFood();
+
+            FoodItems  = new List<SelectListItem>();
+
+            food.ForEach(x =>
+            {
+                FoodItems.Add(new SelectListItem { Value = x.Id.ToString(), Text = x.Title });
+            });
+        }
+    }
+}
