ef5f9b1f51a5756d205e39d0545856dbdda22327
Eric Carlier <eric.carlier@gmail.com>
2021-10-12 09:32:29 +0200

    Update Order box styling

--- RPDemo/Models/OrderUpdateModel.cs
+++ RPDemo/Models/OrderUpdateModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace RPDemo.Models
 {
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+        
+        [DisplayName("Updated Order Name")]
         public string OrderName { get; set; }
     }
 }
