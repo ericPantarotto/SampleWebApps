7c33aee08d827f337f300425281f4560476d035c
Eric Carlier <eric.carlier@gmail.com>
2021-10-13 15:07:40 +0200

    Adding connectionstring

--- MVCDemo/Startup.cs
+++ MVCDemo/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DataLibraryRepo.Data;
+using BillTimeLibrary.RepoDB;
 
 namespace MVCDemo
 {
@@ -24,6 +26,11 @@ namespace MVCDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSingleton<IFoodData, FoodData>();
+            services.AddSingleton<IOrderData, OrderData>();
+
+            new ModelMapper().ModelMap();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
