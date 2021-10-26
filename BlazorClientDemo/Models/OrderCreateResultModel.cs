b6320827cbe6f482cee6f1c9a7f9c2b82d63c903
Eric Carlier <eric.carlier@gmail.com>
2021-10-26 11:55:33 +0200

    BC Create

---
+++ BlazorClientDemo/Models/OrderCreateResultModel.cs
@@ -0,0 +1,8 @@
+
+namespace BlazorClientDemo.Models
+{
+    public class OrderCreateResultModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BlazorClientDemo/Models/OrderUpdateModel.cs
@@ -0,0 +1,8 @@
+namespace BlazorClientDemo.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+    }
+}
